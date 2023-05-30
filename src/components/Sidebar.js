07d429e760019a529664344acc7f64877f1bfc38
Nischal Neupane <102789731+Nischal1111@users.noreply.github.com>
2023-05-30 23:58:36 +0545

    Updated

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import "../Css/sidebar.css"
+import HomeIcon from '@mui/icons-material/Home';
+import SearchIcon from '@mui/icons-material/Search';
+import LibraryMusicIcon from '@mui/icons-material/LibraryMusic';
+import Menu from './Menu'
 
 function Sidebar() {
   return (
     <div className='sidebar'>
     <img className='logo' src='https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg' alt='Spotify'></img>
-    
+    <Menu Icon={HomeIcon} title="Home"/>
+    <Menu Icon={SearchIcon} title="Search"/>
+    <Menu Icon={LibraryMusicIcon} title="Your Library"/>
+    <br></br>
+    <strong className='sidebar--play'>PLAYLISTS</strong>
+    <hr/>
+    <Sidebar title="hipHop"/>
     </div>
   )
 }
